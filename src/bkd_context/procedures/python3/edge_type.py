338525ee7f2569bb3d5918c50109afd8334f56d2
Daniela Pinto Veizaga <danielapintoveizaga@Danielas-MacBook-Pro.local>
2025-02-22 17:37:47 -0500

    more procedures

---
+++ src/bkd_context/procedures/python3/edge_type.py
@@ -0,0 +1,6 @@
+import src.bkd_context.bkd.query_indra
+
+from src.bkd_context.bkd.query_indra import relationships
+
+
+edges = relationships({{ results }})
---
+++ src/bkd_context/procedures/python3/excerpt_extract.py
@@ -0,0 +1,10 @@
+import src.bkd_context.bkd.query_indra
+
+from src.bkd_context.bkd.query_indra import excerpts
+
+# nodes_lists = [
+#     ["CTNNB1", "CDH1"],
+# ]
+# nodes_lists
+
+text = excerpts({{ results }})
