50da3e2fb66594e474cbd32d4acd40337efc69b0
Daniela Pinto Veizaga <danielapintoveizaga@10-18-208-7.dynapool.wireless.nyu.edu>
2025-02-26 23:46:35 -0500

    updates procedures

--- src/bkd_context/procedures/python3/query_gene_pair.py
+++ src/bkd_context/procedures/python3/query_gene_pair.py
@@ -1,9 +1,15 @@
 import src.bkd_context.bkd.query_indra
 import pandas as pd
 from src.bkd_context.bkd.query_indra import bulk_edges
-# nodes_lists = [
-#     ["CTNNB1", "CDH1"],
+from itertools import combinations
+
+# genes =  [
+# ('NOTUM', 'VWA2'), ('NOTUM', 'CTNNB1'),
+# ('NOTUM', 'VPS26C'),
+# ('VWA2', 'CTNNB1'),
+# ('VWA2', 'VPS26C'),
+# ('CTNNB1', 'VPS26C')
 # ]
-# nodes_lists
-edges = bulk_edges([{{ gene_pair }}])
-pd.DataFrame(edges)
+
+edge_data = bulk_edges([{{ genes }}])
+edge_data
