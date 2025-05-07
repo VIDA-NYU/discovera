79214260983a715b83b3ecd3b3d8084b91fefc64
Aécio Santos <aecio.solando@gmail.com>
2025-05-07 18:32:49 -0400

    Fix quotes in rank_gsea function

--- src/bkd_context/procedures/python3/gsea_pipe.py
+++ src/bkd_context/procedures/python3/gsea_pipe.py
@@ -3,7 +3,7 @@ from src.bkd_context.bkd.gsea import rank_gsea
 
 leading_genes = rank_gsea(
     {{ dataset }}, 
-    gene_sets={{ gene_sets }},
+    gene_sets="{{ gene_sets }}",
     hit_col="{{ hit_col }}", 
     corr_col="{{ corr_col }}",
     min_size={{ min_size }}, 
